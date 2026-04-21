1f49e1280c5c7092b85846c3872988f7e9442657
puedesleerlo <atabaresa@unal.edu.co>
2026-04-20 21:38:39 -0500

    Add: design-hashset - submission-8

---
+++ Data Structures & Algorithms/design-hashset/submission-8.py
@@ -0,0 +1,27 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.maxsize = 10000
+        self.hashtable = [[] for i in range(self.maxsize)]
+
+    def add(self, key: int) -> None:
+        ss = key % self.maxsize
+        if key not in self.hashtable[ss]:
+            self.hashtable[ss].append(key)
+
+    def remove(self, key: int) -> None:
+        ss = key % self.maxsize
+        cc = self.contains(key)
+        if cc:
+            self.hashtable[ss].remove(key)
+
+    def contains(self, key: int) -> bool:
+        ss = key % self.maxsize
+        return self.hashtable[ss].count(key) > 0
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
