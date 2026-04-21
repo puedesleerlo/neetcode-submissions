65452915c2eadb5b8038e4c3fe0624050bcf5b3c
puedesleerlo <atabaresa@unal.edu.co>
2026-04-20 19:59:35 -0500

    Add: longest-common-prefix - submission-35

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-35.py
@@ -0,0 +1,10 @@
+from functools import reduce
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        minii = min(strs, key=lambda x: len(x))
+        hol = [all([minii[:i] in item for item in strs]) for i in range(1, len(minii) + 1)]
+        print(hol)
+        allss = sorted([minii[:i] for i in range(1, len(minii)+1)  if all([minii[:i] in item for item in strs])], reverse=True)
+        print(allss)
+        return allss[0] if len(allss) else ""
+
