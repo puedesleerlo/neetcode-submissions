d16d13a7ab8dc94ccbb4cc6327df9ca71c94428a
puedesleerlo <atabaresa@unal.edu.co>
2026-04-22 23:45:25 -0500

    Add: top-k-elements-in-list - submission-30

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-30.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hastable = {}
+        for num in nums:
+            if num in hastable:
+                hastable[num] += 1
+            else:
+                hastable[num] = 1
+        # reversed_hastable = {}
+        buckets = [[] for _ in range(len(nums) + 1)]
+        for ii, obj in hastable.items():
+            buckets[obj].append(ii)
+        clean = [it for i in range(len(buckets)-1, 0, -1) for it in buckets[i]]
+
+        return clean[:k]
